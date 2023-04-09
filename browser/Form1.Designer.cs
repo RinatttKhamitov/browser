d1fc5449cfd85f206b16cf819799e871fca1897c
Ринат Хамитов <stoneaz@mail.ru>
2023-04-09 22:28:05 +0300

    Добавьте файлы проекта.

---
+++ browser/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace browser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnNewPage = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.btnFront = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnDeletePage = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btnDeletePage);
+            this.panel1.Controls.Add(this.btnNewPage);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.textBoxSearch);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnFront);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Location = new System.Drawing.Point(6, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(795, 51);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnNewPage
+            // 
+            this.btnNewPage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnNewPage.Location = new System.Drawing.Point(103, 7);
+            this.btnNewPage.Name = "btnNewPage";
+            this.btnNewPage.Size = new System.Drawing.Size(28, 28);
+            this.btnNewPage.TabIndex = 6;
+            this.btnNewPage.Text = "+";
+            this.btnNewPage.UseVisualStyleBackColor = true;
+            this.btnNewPage.Click += new System.EventHandler(this.btnNewPage_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(720, 7);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(71, 28);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Настройки";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSearch.Location = new System.Drawing.Point(684, 7);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(30, 28);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "🔎";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxSearch.Location = new System.Drawing.Point(171, 12);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(507, 20);
+            this.textBoxSearch.TabIndex = 3;
+            // 
+            // btnFront
+            // 
+            this.btnFront.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnFront.Location = new System.Drawing.Point(37, 7);
+            this.btnFront.Name = "btnFront";
+            this.btnFront.Size = new System.Drawing.Size(28, 28);
+            this.btnFront.TabIndex = 2;
+            this.btnFront.Text = "→";
+            this.btnFront.UseVisualStyleBackColor = true;
+            this.btnFront.Click += new System.EventHandler(this.btnFront_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnUpdate.Location = new System.Drawing.Point(70, 7);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(28, 28);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "↻";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnBack.Location = new System.Drawing.Point(4, 7);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(28, 28);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "←";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(3, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(795, 406);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(787, 380);
+            this.tabPage1.TabIndex = 2;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnDeletePage
+            // 
+            this.btnDeletePage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDeletePage.Location = new System.Drawing.Point(138, 7);
+            this.btnDeletePage.Name = "btnDeletePage";
+            this.btnDeletePage.Size = new System.Drawing.Size(28, 28);
+            this.btnDeletePage.TabIndex = 7;
+            this.btnDeletePage.Text = "x";
+            this.btnDeletePage.UseVisualStyleBackColor = true;
+            this.btnDeletePage.Click += new System.EventHandler(this.btnDeletePage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 457);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button btnFront;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnNewPage;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnDeletePage;
+    }
+}
+
---
+++ browser/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace browser
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitializeChromium();
+
+            CreateNewPage("https://www.google.com");
+
+        }
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+
+
+            // SelectedTab
+        }
+        public void CreateNewPage(string link)
+        {
+            TabPage newTabPage = new TabPage();
+            newTabPage.Text = "Новая вкладка";
+
+            tabControl1.TabPages.Add(newTabPage);
+            tabControl1.SelectedIndex = tabControl1.TabCount - 1;
+
+            ChromiumWebBrowser chrome;
+            chrome = new ChromiumWebBrowser(link);
+            // Add it to the form and fill it to the form window.
+            tabControl1.SelectedTab.Controls.Add(chrome);
+            chrome.Dock = DockStyle.Fill;
+        }
+        private void btnNewPage_Click(object sender, EventArgs e)
+        {
+            CreateNewPage("https://www.google.com");
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser rtb = (ChromiumWebBrowser)tabControl1.SelectedTab.Controls.Cast<Control>().FirstOrDefault(x => x is ChromiumWebBrowser);
+            rtb.Back();
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser rtb = (ChromiumWebBrowser)tabControl1.SelectedTab.Controls.Cast<Control>().FirstOrDefault(x => x is ChromiumWebBrowser);
+            rtb.Reload();
+        }
+
+        private void btnFront_Click(object sender, EventArgs e)
+        {
+            ChromiumWebBrowser rtb = (ChromiumWebBrowser)tabControl1.SelectedTab.Controls.Cast<Control>().FirstOrDefault(x => x is ChromiumWebBrowser);
+            rtb.Forward();
+        }
+
+        private void btnDeletePage_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //https://www.google.com/search?q=123
+            if (textBoxSearch.Text.Contains("http://") || textBoxSearch.Text.Contains("https://"))
+            {
+                CreateNewPage(textBoxSearch.Text);
+            }
+            else
+            {
+                CreateNewPage($"https://www.google.com/search?q={textBoxSearch.Text}");
+            }
+            textBoxSearch.Text = string.Empty;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
